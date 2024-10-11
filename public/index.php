f5907e366be805284f920974bb8445816b6d253d
wubshet <0953895891w@gmail.com>
2024-10-11 13:15:58 +0300

    i have deleted the local bootstrap file and change some codes in index .php file

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@
   <title>Belsty Jemberie</title>
   
   <!-- Bootstrap CSS -->
-  <link href="bootstrap/bootstrap-5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
   <!-- Custom CSS -->
   <link rel="stylesheet" href="css/styles.css">
@@ -166,6 +166,6 @@
   ?>
 
   <!-- Bootstrap JS and Dependencies -->
-  <script src="bootstrap/bootstrap-5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
